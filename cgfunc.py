0d71900dae0415e75843cd0823b8855f68bf9658
TwanvanDam <twanvandam02@gmail.com>
2024-03-29 13:09:21 +0100

    Make cg_calc nicer

--- cgfunc.py
+++ cgfunc.py
@@ -2,8 +2,7 @@
 def cg_calc(data: dict):
     sum_weight = 0
     sum_sum = 0
-    for key in data.keys():
-        item = data[key]
+    for key, item in data.items():
         sum_sum += item[0] * item[1]
         sum_weight = item[0]
     return sum_sum / sum_weight
